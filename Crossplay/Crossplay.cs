85fac6c1aadbf966b9e9fd5a66811e7edb2efd3b
Moneylover3246 <66443019+Moneylover3246@users.noreply.github.com>
2021-06-24 12:58:59 -0400

    Version tick to 1.3.3

--- Crossplay/Crossplay.cs
+++ Crossplay/Crossplay.cs
@@ -17,7 +17,7 @@ namespace Crossplay
         public override string Name => "Crossplay";
         public override string Author => "Moneylover3246";
         public override string Description => "Enables crossplay for terraria";
-        public override Version Version => new Version("1.3.2");
+        public override Version Version => new Version("1.3.3");
 
         internal static ConfigFile Config = new ConfigFile();
 
